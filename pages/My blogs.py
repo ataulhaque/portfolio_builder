7f70099ff706a318eb523547a004636f857c89b5
ataulhaque <atauldilse@gmail.com>
2023-12-06 23:15:12 +0530

    changed for Ataul

--- pages/My blogs.py
+++ pages/My blogs.py
@@ -9,8 +9,6 @@ df['category1'] = df.apply(lambda x:x['category1'].split('|'),axis=1)
 df = df.explode('category1')
 grouped = df.groupby(['category1']).agg(list)
 grouped['total'] = grouped['url'].transform(len)
-grouped.at['Other technical','total'] = 0
-grouped.at['Other non technical','total'] = 0
 grouped = grouped.sort_values(by='total',ascending=False)
 for x,y in grouped.iterrows():
     with st.expander(x.upper()):
--- streamlit_app.py
+++ streamlit_app.py
@@ -17,16 +17,16 @@ from graph_builder import *
 #import tensorflow as tf
 from streamlit_player import st_player
 
-st.set_page_config(page_title='mehul gupta\'s portfolio' ,layout="wide",page_icon='👨‍🔬')
+st.set_page_config(page_title="Ataul Haque's Portfolio",
+                   layout="wide",
+                   page_icon='👨‍🔬'
+                   )
 
 st.sidebar.markdown(info['Stackoverflow_flair'],unsafe_allow_html=True)
 st.subheader('Summary')
 st.write(info['Brief'])
 
-
 st.subheader('Career snapshot')
-
-
     
 with st.spinner(text="Building line"):
     with open('timeline.json', "r") as f:
@@ -55,15 +55,14 @@ st.subheader('Education 📖')
 
 fig = go.Figure(data=[go.Table(
     header=dict(values=list(info['edu'].columns),
-                fill_color='paleturquoise',
+                fill_color='black',
                 align='left',height=65,font_size=20),
     cells=dict(values=info['edu'].transpose().values.tolist(),
-               fill_color='lavender',
+               fill_color='darkgrey',
                align='left',height=40,font_size=15))])
 
 fig.update_layout(width=750, height=400)
 st.plotly_chart(fig)
-st.subheader('Research Papers 📝')
 
 def plot_bar():
     
@@ -105,70 +104,13 @@ def image_and_status_loader(image_list,index=0):
         
         plot_bar()
         
-        
-
-def paper_summary(index):
-    st.markdown('<h5><u>'+paper_info['name'][index]+'</h5>',unsafe_allow_html=True)
-    st.caption(paper_info['role'][index])
-    st.caption(paper_info['journal'][index]+' , '+paper_info['publication'][index]+' , '+paper_info['year'][index])
-    with st.expander('detailed description'):
-        with st.spinner(text="Loading details..."):
-                st.write(paper_info['Summary'][index])
-                pdfFileObj = open('pdfs/{}'.format(paper_info['file'][index]), 'rb')
-                image_and_status_loader(paper_info['images'][str(index)], index)
-                if index==0:
-                    rpa_metrics['time_improvement'] = rpa_metrics['non-ds']-rpa_metrics['ds']
-                    st.markdown('**Time taken per order involving Rx in seconds** (green indicates improvements from baseline)')
-                    cols = st.columns(3)
-                    for index_, row in rpa_metrics.iterrows():
-                        cols[index_].metric(row['category'],str(row['ds'])+'s',delta=str(round(row['time_improvement'],1))+'s' )
-                st.download_button('download paper',pdfFileObj,file_name=paper_info['file'][index],mime='pdf')
-    
-
-
-paper_summary(0)
-paper_summary(1)
-
-st.subheader('Achievements 🥇')
-achievement_list = ''.join(['<li>'+item+'</li>' for item in info['achievements']])
-st.markdown('<ul>'+achievement_list+'</ul>',unsafe_allow_html=True)
-
-
-st.subheader('Medium Profile ✍️')
-st.markdown("""<a href={}> access full profile here</a>""".format(info['Medium']),unsafe_allow_html=True)
-
-try:
-        page1,page2 = requests.get(info['Medium']), requests.get(info['publication_url'])
-        
-        followers = re.findall('(\d+\.\d+[kK]?) Followers',page1.text)[0]
-        pub_followers = re.findall('Followers (?:\w+\s+){4}(\d+)',re.sub('\W+',' ', page2.text ))[0]
-        
-        cols = st.columns(2)
-        cols[0].metric('Followers',followers)
-        cols[1].metric('Publication followers',pub_followers)
-except:
-    pass
-
-with st.expander('read my latest blogs below'):
-    components.html(embed_component['medium'],height=500)
-
-st.subheader('Youtube ▶️')
-st.markdown("""<a href={}> access channel here</a>""".format(info['youtube_url']),unsafe_allow_html=True)
-page1,page2 = requests.get(info['youtube_url']), requests.get(info['youtube_about'])
-subs = re.findall('(\d+\.\d+[kK]?) subscribers',page1.text)[0]
-videos = re.findall( r'"videosCountText".*?"text":"(\d+)"',page1.text)[0]
-
-cols = st.columns(2)
-cols[0].metric('Subscribers',subs)
-cols[1].metric('Videos',videos)
-        
-st.subheader('Daily routine as Data Scientist')
+st.subheader('Daily routine as AI/ML Researcher')
 st.graphviz_chart(graph)
 
 st.sidebar.caption('Wish to connect?')
-st.sidebar.write('📧: mehulgupta2016154@gmail.com')
-pdfFileObj = open('pdfs/mehul_gupta_resume.pdf', 'rb')
-st.sidebar.download_button('download resume',pdfFileObj,file_name='mehul_gupta_resume.pdf',mime='pdf')
+st.sidebar.write('📧: atauldilse@gmail.com')
+pdfFileObj = open('pdfs/CV_Ataul Haque.pdf', 'rb')
+st.sidebar.download_button('download resume',pdfFileObj,file_name='CV_Ataul Haque.pdf',mime='pdf')
 
 
 
